a41b9bd1b94e81a96ba9ca42843a444fa61d9306
SHjNEe <baotrung.jpvn@gmail.com>
2023-03-23 22:33:14 +0700

    Update

--- src/components/KnowledgeBase.vue
+++ src/components/KnowledgeBase.vue
@@ -1,10 +1,15 @@
 <template>
   <section>
     <h2>Select a Topic</h2>
+    <!-- <knowledge-grid
+      @select-topic="$emit('select-topic', $event)"
+    ></knowledge-grid> -->
     <knowledge-grid></knowledge-grid>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  // emits: ['select-topic'],
+};
 </script>
