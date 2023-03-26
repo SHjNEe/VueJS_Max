79f457c17ca48a891b94cc946f77fca779a8d1f5
SHjNEe <baotrung.jpvn@gmail.com>
2023-03-26 23:13:40 +0700

    add

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <!-- <the-header></the-header> -->
+    <TheHeader />
+    <BadgeList />
+    <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    />
+    <CourseGoals #default="slotProps">
+      <!-- <template #default="slotProps"> -->
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps["another-prop"] }}</p>
+      <!-- </template> -->
+    </CourseGoals>
   </div>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+    CourseGoals,
+    // "the-header": TheHeader,
+  },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
