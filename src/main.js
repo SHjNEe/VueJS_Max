9e167abb6184f34185605dc58f30fd1875bf34e6
SHjNEe <baotrung.jpvn@gmail.com>
2023-03-27 00:34:38 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-import { createApp } from "vue";
+import { createApp } from 'vue';
 
-import App from "./App.vue";
-import BaseBadge from "./components/UI/BaseBadge.vue";
-import BaseCard from "./components/UI/BaseCard.vue";
-
-const app = createApp(App);
-
-app.component("base-badge", BaseBadge);
-app.component("base-card", BaseCard);
-
-app.mount("#app");
+createApp({}).mount('#app');
