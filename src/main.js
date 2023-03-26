cfb51ec4d58acca14b86a1762ebfa9b75ff13c6f
SHjNEe <baotrung.jpvn@gmail.com>
2023-03-26 17:37:35 +0700

    ADd

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import ActiveElement from './components/ActiveElement.vue';
-import KnowledgeBase from './components/KnowledgeBase.vue';
-import KnowledgeElement from './components/KnowledgeElement.vue';
-import KnowledgeGrid from './components/KnowledgeGrid.vue';
+import TheHeader from './components/TheHeader.vue';
+import BaseBadge from './components/BaseBadge.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
 
 const app = createApp(App);
 
-app.component('active-element', ActiveElement);
-app.component('knowledge-base', KnowledgeBase);
-app.component('knowledge-element', KnowledgeElement);
-app.component('knowledge-grid', KnowledgeGrid);
+app.component('the-header', TheHeader);
+app.component('base-badge', BaseBadge);
+app.component('badge-list', BadgeList);
+app.component('user-info', UserInfo);
 
 app.mount('#app');
