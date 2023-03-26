7aa0bf51ab4e12ff2fc2cfb6e76bee5fa3532728
SHjNEe <baotrung.jpvn@gmail.com>
2023-03-27 01:09:41 +0700

    Update

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <StoredResource :resource="storedResources" />
+</template>
+
+<script>
+import StoredResource from "./components/learning-resource/StoredResource.vue";
+export default {
+  components: {
+    StoredResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The offical Vue.js documentation",
+          link: "https://vue.js",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "The Google search page",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
