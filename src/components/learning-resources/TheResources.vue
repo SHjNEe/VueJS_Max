9b807e81e82d1f6ebf2d350e24bfc637e28bb8d3
SHjNEe <baotrung.jpvn@gmail.com>
2023-03-28 00:56:47 +0700

    add

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,17 +1,24 @@
 <template>
-  <base-card>
-    <base-button
-      @click="setSelectedTab('stored-resources')"
-      :mode="storedResButtonMode"
-    >Stored Resources</base-button>
-    <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
-  </base-card>
-  <component :is="selectedTab"></component>
+  <div>
+    <base-card>
+      <base-button
+        @click="setSelectedTab('stored-resources')"
+        :mode="storedResButtonMode"
+        >Stored Resources</base-button
+      >
+      <base-button
+        @click="setSelectedTab('add-resource')"
+        :mode="addResButtonMode"
+        >Add Resource</base-button
+      >
+    </base-card>
+    <component :is="selectedTab"></component>
+  </div>
 </template>
 
 <script>
-import StoredResources from './StoredResources.vue';
-import AddResource from './AddResource.vue';
+import StoredResources from "./StoredResources.vue";
+import AddResource from "./AddResource.vue";
 
 export default {
   components: {
@@ -20,19 +27,19 @@ export default {
   },
   data() {
     return {
-      selectedTab: 'stored-resources',
+      selectedTab: "stored-resources",
       storedResources: [
         {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation.',
-          link: 'https://vuejs.org',
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation.",
+          link: "https://vuejs.org",
         },
         {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://google.org',
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://google.org",
         },
       ],
     };
@@ -44,10 +51,10 @@ export default {
   },
   computed: {
     storedResButtonMode() {
-      return this.selectedTab === 'stored-resources' ? null : 'flat';
+      return this.selectedTab === "stored-resources" ? null : "flat";
     },
     addResButtonMode() {
-      return this.selectedTab === 'add-resource' ? null : 'flat';
+      return this.selectedTab === "add-resource" ? null : "flat";
     },
   },
   methods: {
