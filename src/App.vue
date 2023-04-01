58418d9cd219b00dc15a2fd79265014cb51e3f5d
SHjNEe <baotrung.jpvn@gmail.com>
2023-04-02 00:20:56 +0700

    Add

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,39 @@
 <template>
-  <div>
-    <learning-survey></learning-survey>
-    <user-experiences></user-experiences>
-  </div>
+  <the-navigation></the-navigation>
+  <main>
+    <router-view></router-view>
+  </main>
 </template>
 
 <script>
-import LearningSurvey from "./components/survey/LearningSurvey.vue";
-import UserExperiences from "./components/survey/UserExperiences.vue";
+import TheNavigation from './components/nav/TheNavigation.vue';
 
 export default {
   components: {
-    LearningSurvey,
-    UserExperiences,
+    TheNavigation
   },
   data() {
     return {
-      savedSurveyResults: [],
+      teams: [
+        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
+        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
+        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
+      ],
+      users: [
+        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
+        { id: 'u2', fullName: 'Praveen Kumar', role: 'Engineer' },
+        { id: 'u3', fullName: 'Julie Jones', role: 'Engineer' },
+        { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
+        { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
+      ],
     };
   },
-  methods: {
-    storeSurvey(surveyData) {
-      const surveyResult = {
-        name: surveyData.userName,
-        rating: surveyData.rating,
-        id: new Date().toISOString(),
-      };
-      this.savedSurveyResults.push(surveyResult);
-      console.log(surveyResult);
-    },
-  },
+  provide() {
+    return {
+      teams: this.teams,
+      users: this.users,
+    };
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-import BaseCard from './components/UI/BaseCard.vue';
-import BaseButton from './components/UI/BaseButton.vue';
-import App from './App.vue';
+import App from "./App.vue";
+import TeamsList from "./components/teams/TeamsList.vue";
+import UsersList from "./components/users/UsersList.vue";
+import TeamMembers from "./components/teams/TeamMembers.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/teams", component: TeamsList }, // our-domain.com/teams => TeamsList
+    { path: "/users", component: UsersList },
+    { path: "/teams/:teamId", component: TeamMembers },
+  ],
+  linkActiveClass: "active",
+});
 
 const app = createApp(App);
 
-app.component('base-card', BaseCard);
-app.component('base-button', BaseButton);
+app.use(router);
 
-app.mount('#app');
+app.mount("#app");
